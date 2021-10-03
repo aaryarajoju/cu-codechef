142ee2ee357d517b22b560382205521caa1ce5cd
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2021-10-03 17:52:56 +0530

    Delete 52002263.py

--- CUPP2101/SNDMAX/52002263.py
+++
@@ -1,9 +0,0 @@
-for _ in range (int(input())):
-    (n, k) = map(int, input().split(' '))
-    
-    maxCoins = 0
-    for i in range(1, k+1):
-        coins = n - (i * (n//i))
-        maxCoins = max(maxCoins, coins)
-    
-    print(maxCoins)
